e96a06e32decc96b13e35af1226c9c016ad51abc
Adam Nolan <anolan@apterainc.com>
2018-08-10 14:08:12 -0400

    Initial Commit

---
+++ GreetingKata/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GreetingKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Greeter 
+    {
+        private bool IsNotNull(string s) => s != null;
+        private bool IsAllCaps(string s) => s.ToUpper() == s;
+
+        public string Greet(string name)
+        {
+            if (IsNotNull(name) && IsAllCaps(name))
+                return $"HELLO {name}!";
+            return $"Hello, {(IsNotNull(name) ? name : "My Friend")}";
+        }
+    }
+}
---
+++ GreetingKataTests/GreeterTests.cs
@@ -0,0 +1,40 @@
+using System;
+using GreetingKata;
+using NUnit.Framework;
+
+namespace GreetingKataTests
+{
+
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void  Greeter_hould_Return_String_Of_Hello_Plus_Input()
+        {
+            var name = "Some Name";
+            var greeter = new Greeter();
+            var result = greeter.Greet(name);
+
+            Assert.AreEqual("Hello, " + name, result);
+        }
+
+        [Test]
+        public void Gretter_Should_Return_My_Friend_When_Name_Is_Null()
+        {
+            var g = new Greeter();
+            var result = g.Greet(null);
+
+            Assert.AreEqual("Hello, My Friend", result);
+        }
+
+        [Test]
+        public void Greeter_Should_Yell_Back_When_Name_Is_All_Caps()
+        {
+            var name = "DANNY BOY";
+            var g = new Greeter();
+            var result = g.Greet(name);
+
+            Assert.AreEqual($"HELLO {name}!", result);
+        }
+    }
+}
