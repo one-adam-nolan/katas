c719a5476747dfd5a777079fc32e9817a992dd6d
Adam Nolan <anolan@apterainc.com>
2018-09-11 10:23:24 -0400

    FizzBuzz:  Add And Refactor

---
+++ KataTests/FizzBuzz/FizzBuzzTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Katas.FizzBuzz;
+using NUnit.Framework;
+
+namespace KataTest.FizzBuzz
+{
+    [TestFixture]
+    public class FizzBuzzTest
+    {
+        private FizzBuzzGenerator _fizzBuzz;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fizzBuzz = new FizzBuzzGenerator();
+        }
+
+        [Test]
+        public void Should_Return_Numbers_One_To_One_Hundred()
+        {
+            int[] result = _fizzBuzz.GetNumberList();
+
+            var isSorted = true;
+
+            for (var i = 0; i < result.Length; ++i)
+            {
+                if (i + 1 < result.Length && result[i + 1] != result[i] + 1)
+                    isSorted = false;
+            }
+
+            Assert.IsTrue(isSorted);
+            Assert.AreEqual(result, Enumerable.Range(1, 100).ToArray());
+        }
+
+        [Test]
+        public void Should_Convert_To_String_Printing_Fizz_And_Buzz()
+        {
+            var numbers = _fizzBuzz.GetNumberList();
+            var result = _fizzBuzz.FizzAndBuzz(numbers);
+
+            Assert.IsFalse(result.Any(n => int.TryParse(n, out int i) && i % 3 == 0));
+            Assert.IsFalse(result.Any(n => int.TryParse(n, out int i) && i % 5 == 0));
+        }
+    }
+}
---
+++ src/Katas/FizzBuzz/FizzBuzzGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Katas.FizzBuzz
+{
+    public class FizzBuzzGenerator
+    {
+        public int[] GetNumberList() => Enumerable.Range(1, 100).ToArray();
+
+        public string[] FizzAndBuzz(int[] numbers)
+        {
+            var numberList = numbers.ToList();
+            return numberList.Select(ConvertToString).ToArray();
+        }
+
+        private string ConvertToString(int num)
+        {
+            var output = "";
+
+            if (IsDivisibleByThree(num))
+                output += "fizz";
+            if (IsDivisibleByFive(num))
+                output += "buzz";
+
+            return string.IsNullOrEmpty(output) ? num.ToString() : output;
+        }
+
+        private bool IsDivisibleByThree(int num) => num % 3 == 0;
+        private bool IsDivisibleByFive(int num) => num % 5 == 0;
+    }
+}
